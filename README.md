7a40f59d9e862cbaa08260c2fe36bcc27c37d83d
Anonoei <dev@anonoei.com>
2024-11-29 14:43:14 -1000

    Initial working

---
+++ README.md
@@ -0,0 +1,12 @@
+# anosway
+ My sway configuration
+
+# Dependencies
+ - [sway](https://github.com/swaywm/sway)
+ - AUR: [tofi](https://github.com/philj56/tofi)
+ - AUR: [mako](https://github.com/emersion/mako)
+ - [workstyle](https://github.com/pierrechevalier83/workstyle) - workspaces
+ - [waybar](https://github.com/Alexays/Waybar/) - status bar
+   - otf-font-awesome
+ - [swaylock](https://github.com/swaywm/swaylock)
+ - [swayrst](https://github.com/Nama/swayrst) - session management
---
+++ wsgrid.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Get current workspace number:
+#current_ws=$(swaymsg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
+
+if [[ "$1" == "-t" ]]; then
+    current_ws=$2
+    direction=$3
+    action="n"
+else
+    current_ws=$(swaymsg -p -t get_workspaces | grep "focused" | cut -d" " -f2 | cut -d":" -f1)
+    #
+    # Direction cases are Left, Right, Up, Down
+    # Get "direction" supplied as argument $1 by keybinding
+    case $1 in
+        m|move)
+            action="m"
+            ;;
+        w|workspace)
+            action="w"
+            ;;
+    esac
+    direction=$2
+fi
+
+# 1 2 3
+# 4 5 6
+# 7 8 9
+echo "Current WS: '$current_ws'"
+echo "Action: '$action'"
+echo "Direction: '$direction'"
+
+int_ws=""
+
+case $direction in
+    l|left)
+        int_ws=$(($current_ws - 1))
+        if [[ "$int_ws" == "0" ]]; then
+            int_ws=9
+        fi
+        ;;
+    r|right)
+        int_ws=$(($current_ws + 1))
+        if [[ "$int_ws" == "10" ]]; then
+            int_ws=1
+        fi
+        ;;
+    u|up)
+        int_ws=$(($current_ws + 3))
+        if [[ $int_ws -gt 9 ]]; then
+            int_ws=$(($int_ws - 9))
+        fi
+        ;;
+    d|down)
+        int_ws=$(($current_ws - 3))
+        if [[ $int_ws -lt 0 ]]; then
+            int_ws=$(($int_ws + 9))
+        fi
+        ;;
+esac
+
+echo ""
+echo "Got WS: '$int_ws'"
+
+
+case $action in
+    m)
+        swaymsg move workspace number $int_ws
+        ;;
+    w)
+        swaymsg workspace number $int_ws
+        ;;
+esac
